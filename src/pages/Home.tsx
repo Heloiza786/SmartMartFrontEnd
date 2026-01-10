10790b7b28ae7861a0f4cd8e14b338718529eb12
Heloiza786 <heloizacustodio786@gmail.com>
2026-01-09 23:59:21 -0300

    feat: new style homePage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,57 @@
 import { Link } from 'react-router-dom'
 import { ShoppingCart, Folder, DollarSign } from 'lucide-react'
-import Header from '../components/Header'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="min-h-screen bg-blue-90">
-      {/* Header com logo */}
-      <Header />
+    <div className="w-full min-h-screen flex items-center justify-between p-6 bg-gray-100 gap-6">
 
-      {/* Conteúdo central */}
-      <div className="flex flex-col items-center justify-center gap-10 mt-8">
-        <h2 className="text-3xl font-bold text-gray-800">Bem-vindo ao SmartMart seu sistema de gerenciamento de estoque </h2>
-
-        <div className="flex gap-10">
+    
+      <div className="flex flex-col gap-6 max-w-lg">
+        <h1 className="text-4xl font-bold text-gray-800">
+          Bem-vindo ao SmartMart
+        </h1>
+        <p className="text-gray-600 text-lg">
+          Seu sistema de gerenciamento de estoque
+        </p>
+          <p className="text-1xl font-bold text-gray-800">
+           Prezando sempre pela segurança, confiabilidade e economia de seus parceiros.
+        </p>
+ 
+       
+        <div className="flex flex-col sm:flex-row gap-4 mt-4">
           <Link
             to="/produtos"
-            className="flex flex-col items-center text-gray-700 hover:text-blue-600 transition"
+            className="flex items-center gap-2 px-6 py-3 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-700 transition"
           >
-            <div className="w-20 h-20 flex justify-center items-center bg-blue-100 rounded-xl shadow-md">
-              <ShoppingCart className="w-10 h-10" />
-            </div>
-            <span className="mt-2 font-semibold">Produtos</span>
+            <ShoppingCart className="w-5 h-5" />
+            Produtos
           </Link>
 
           <Link
             to="/categorias"
-            className="flex flex-col items-center text-gray-700 hover:text-green-600 transition"
+            className="flex items-center gap-2 px-6 py-3 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-700 transition"
           >
-            <div className="w-20 h-20 flex justify-center items-center bg-blue-100 rounded-xl shadow-md">
-              <Folder className="w-10 h-10" />
-            </div>
-            <span className="mt-2 font-semibold">Categorias</span>
+            <Folder className="w-5 h-5" />
+            Categorias
           </Link>
 
           <Link
             to="/vendas"
-            className="flex flex-col items-center text-gray-700 hover:text-purple-600 transition"
+            className="flex items-center gap-2 px-6 py-3 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-700 transition"
           >
-            <div className="w-20 h-20 flex justify-center items-center bg-blue-100 rounded-xl shadow-md">
-              <DollarSign className="w-10 h-10" />
-            </div>
-            <span className="mt-2 font-semibold">Vendas</span>
+            <DollarSign className="w-5 h-5" />
+            Vendas
           </Link>
         </div>
+      </div>
+
+      
+      <div className="flex justify-end">
+        <img
+          src="/img/composition.png"
+          alt="Composição"
+          className="w-150 "
+        />
       </div>
     </div>
   )
