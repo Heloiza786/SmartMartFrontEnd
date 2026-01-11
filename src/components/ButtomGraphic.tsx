631e21d78ca5e5cb9290b906a563228dd12849ef
Heloiza786 <heloizacustodio786@gmail.com>
2026-01-11 15:52:51 -0300

    feat: create buttomGraphic

---
+++ src/components/ButtomGraphic.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { BarChart3 } from "lucide-react";
+
+export function ButtomGraphic() {
+  return (
+    <div className="flex flex-col sm:flex-row gap-4 mt-4">
+      <Link
+        to="/chartHome"
+        className="flex items-center gap-2 px-6 py-3 rounded-xl bg-blue-500 text-white font-semibold hover:bg-blue-700 transition"
+      >
+        <BarChart3 className="w-5 h-5" />
+        Gráfico
+      </Link>
+    </div>
+  );
+}
+
+
